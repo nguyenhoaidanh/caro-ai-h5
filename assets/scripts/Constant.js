80e2f656d966ba0b4fad3ec5944fb26f902a0ffa
danh <nguyenhoaidanh2096@gmail.com>
2019-08-31 21:45:22 +0700

    deploy2

--- assets/scripts/Constant.js
+++ assets/scripts/Constant.js
@@ -1,3 +1,4 @@
 window.config = {
-    api_domain: 'http://localhost:3000/api/users'
+   // api_domain: 'http://localhost:3000/api/users'
+    api_domain: 'https://caro-ai-h5.herokuapp.com/api/users'
 }
